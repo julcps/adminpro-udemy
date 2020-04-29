4ecab186c76f6d9f741a8671ef921ee2b1040173
julcps@gmail.com <julcps@gmail.com>
2020-04-29 02:20:18 -0400

    Sección 8 terminada

--- src/app/components/incrementador/incrementador.component.ts
+++ src/app/components/incrementador/incrementador.component.ts
@@ -18,7 +18,7 @@ export class IncrementadorComponent implements OnInit {
 
     // console.log('leyenda', this.leyenda);
     // console.log('progreso', this.progreso);
-  }
+  } 
 
   ngOnInit() {
     // console.log('leyenda', this.leyenda);
@@ -30,7 +30,7 @@ export class IncrementadorComponent implements OnInit {
     // let elemHTML: any = document.getElementsByName('progreso')[0];
     // console.log( elemHTML.value);
 
-    console.log( this.txtProgress);
+    // console.log( this.txtProgress);
 
     if (newValue >= 100) {
       this.progreso = 100;
@@ -43,22 +43,23 @@ export class IncrementadorComponent implements OnInit {
     // elemHTML.value = this.progreso;
     this.txtProgress.nativeElement.value = this.progreso;
 
-    this.cambioValor.emit( this.progreso);
+    this.cambioValor.emit( this.progreso );
   }
 
   cambiarValor( valor: number ) {
-      if ( (this.progreso + valor ) >= 100 ) {
+    if ( this.progreso >= 100 && valor > 0 ) {
         this.progreso = 100;
         return;
       }
-      if ( (this.progreso + valor ) <= 0 ) {
+    if ( this.progreso <= 0 && valor < 0 ) {
         this.progreso = 0;
         return;
       }
-      this.progreso = this.progreso + valor;
 
-      this.cambioValor.emit( this.progreso);
+    this.progreso = this.progreso + valor;
 
-      this.txtProgress.nativeElement.focus();
+    this.cambioValor.emit( this.progreso );
+
+    this.txtProgress.nativeElement.focus();
     }
 }
---
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -0,0 +1,81 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+
+import { retry, map, filter } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-rxjs',
+  templateUrl: './rxjs.component.html',
+  styles: []
+})
+
+export class RxjsComponent implements OnInit, OnDestroy {
+
+  subscription: Subscription;
+
+  constructor() {
+
+    this.subscription = this.regresaObservable()
+      .subscribe(
+        numero => console.log( 'Subs', numero),
+        error => console.error( 'Error en el obs (2 veces)', error ),
+        () => console.log( 'El observador terminó!')
+      );
+
+  }
+
+  ngOnInit() {
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  regresaObservable(): Observable<any> {
+
+    return new Observable( observer => {
+
+      let contador = 0;
+
+      const intervalo = setInterval( () => {
+
+        contador += 1;
+
+        const salida = {
+          valor: contador
+         };
+  
+        observer.next( salida );
+
+        // if ( contador === 3 ) {
+        //   clearInterval( intervalo );
+        //   observer.complete();
+        // }
+
+        // if ( contador === 2 ) {
+        //   observer.error( 'Auxilio!' );
+        // }
+
+      }, 500 );
+
+    }).pipe(retry(2))
+      .pipe(
+        map((resp: any) => {
+         return resp.valor;
+        })
+      )
+      .pipe(
+        filter( (valor, index) => {
+          if ( (valor % 2) === 1) {
+            // impar
+            return true;
+          } else {
+            // par
+            return false;
+          }
+        })
+      );
+
+  }
+
+}
